4f9388cd71cf6b95a7b013414deb7403eb99787d
giacomo9999 <gmarchesi888@gmail.com>
2021-04-23 16:02:01 -0400

    All functionalities now working.

--- app/controllers/fruits.controller.js
+++ app/controllers/fruits.controller.js
@@ -13,6 +13,7 @@ exports.create = (req, res) => {
     fruitName: req.body.fruitName,
     fruitColor: req.body.fruitColor,
     fruitShape: req.body.fruitShape,
+    inStock: req.body.inStock,
   });
 
   console.log("Fruit:", fruit);
@@ -28,6 +29,7 @@ exports.create = (req, res) => {
 };
 
 exports.findAll = (req, res) => {
+  // localhost:8080/api/fruits?fruitName=apple
   const fruitName = req.query.fruitName;
   console.log(req.query);
   // let condition = fruitName
@@ -56,7 +58,57 @@ exports.findOne = (req, res) => {
     });
 };
 
-exports.update = (req, res) => {};
-exports.delete = (req, res) => {};
-exports.deleteAll = (req, res) => {};
-exports.findAllInStock = (req, res) => {};
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({ message: "Data in update cannot be empty." });
+  }
+  const id = req.params.id;
+  Fruit.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: "Cannot update fruit with id " + id });
+      } else res.send({ message: "Fruit updated successfully...id" + id });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error updating fruit." });
+    });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  console.log("Deleting...", id);
+  Fruit.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: "No Data" });
+      } else {
+        res.send({ message: "Fruit " + id + " deleted successfully." });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Cannot delete fruit with ID " + id });
+    });
+};
+
+exports.deleteAll = (req, res) => {
+  console.log("Deleting all fruits...");
+  Fruit.deleteMany({})
+    .then((data) => {
+      res.send({ message: `${data.deletedCount} fruits were deleted.` });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error in clearing database" });
+    });
+};
+
+exports.findAllInStock = (req, res) => {
+  Fruit.find({ inStock: true })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "An error occurred while finding in-stock fruits" });
+    });
+};
--- app/routes/fruits.routes.js
+++ app/routes/fruits.routes.js
@@ -5,10 +5,10 @@ module.exports = (app) => {
 
   // create a new fruit
   router.post("/", fruits.create);
+  router.get("/inStock", fruits.findAllInStock);
   router.get("/", fruits.findAll);
   router.get("/:id", fruits.findOne);
-  //   router.put("/:id", fruits.update);
-  //   router.delete("/:id", fruits.deleteOne);
-  //   router.delete("/", fruits.deleteAll);
-  //   router.get("/inStock", fruits.findOne);
+  router.put("/:id", fruits.update);
+  router.delete("/:id", fruits.delete);
+  router.delete("/", fruits.deleteAll);
 };
